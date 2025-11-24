6ec2073a7b132ca23a80cc0e1f540e1232b7c963
Tzuyang Tsai <14104502+shiyou0130011@users.noreply.github.com>
2025-11-24 13:31:07 +0800

    Modify: 建立初始的 ebiten game

---
+++ game.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 960, 720
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+
+func main() {
+	ebiten.SetWindowSize(960, 720)
+	ebiten.SetWindowTitle("Hello, World!")
+
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
